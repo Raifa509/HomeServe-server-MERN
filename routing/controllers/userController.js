39c0c4c80bf03bbce1a75e752bd03c592d6d8eb3
Raifa509 <fathimathulraifanp@gmail.com>
2025-10-18 07:40:32 +0530

    login done

--- routing/controllers/userController.js
+++ routing/controllers/userController.js
@@ -1,23 +1,23 @@
 const users = require('../../models/userModel')
+const jwt = require('jsonwebtoken')
 
 //register
-exports.registerController =async (req, res) => {
+exports.registerController = async (req, res) => {
     console.log("Inside Register API");
     console.log(req.body);
     const { username, email, password } = req.body
     // console.log(username,email,password);
     try {
-        const existingUser=await users.findOne({email})
-        if(existingUser)
-        {
+        const existingUser = await users.findOne({ email })
+        if (existingUser) {
             res.status(409).json("User Already exist!!! Please Login")
-        }else{
-            const newUser=new users({
+        } else {
+            const newUser = new users({
                 username,
                 email,
                 password
             })
-            
+
             await newUser.save()
             res.status(201).json(newUser)
         }
@@ -29,7 +29,31 @@ exports.registerController =async (req, res) => {
 
 //login
 
- 
+exports.loginController = async (req, res) => {
+    console.log("Inside login controller");
+    // console.log(req.body)
+    const { email, password } = req.body
+    // console.log(email, password);
+    try {
+        const existingUser = await users.findOne({ email })
+        console.log(existingUser);
+
+        if (existingUser) {
+            if (existingUser.password == password) {
+                const token = jwt.sign({ userMail: existingUser.email }, process.env.JWTSECRET)
+                res.status(200).json({ user: existingUser, token })
+            } else {
+                res.status(401).json("Invalid Credential")
+
+            }
+        } else {
+            res.status(404).json("Account doesn't exist!!!")
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+}
+
 
 
 
