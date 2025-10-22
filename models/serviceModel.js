e60385004fe8de6bace73f5919f0efe65b1185f3
Raifa509 <fathimathulraifanp@gmail.com>
2025-10-22 09:08:09 +0530

    servicemodel created

---
+++ models/serviceModel.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+// Schema creation
+const serviceSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        default: 'No Description provided'
+    },
+    about: {
+        type: String,
+        default: ''
+    },
+    category: {
+        type: String,
+        enum: ['Cleaning', 'Gardening', 'Repairs', 'Painting', 'Installation', 'Moving', 'Emergency'],
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    duration: {
+        type: String,
+        default: ''
+    },
+    rating: {
+        type: Number,
+        default: 0,
+        min: 0,
+        max: 5
+    },
+    whatsIncluded: {
+        type: [String],
+        default: []
+    },
+    pricingTiers: [
+        {
+            name: String,
+            price: Number
+        }
+    ],
+    thumbnail: {
+        type: String,
+        default: '',
+    },
+    detailImage: {
+        type: String, 
+        default: '',
+    },
+});
+
+const services = mongoose.model('services', serviceSchema);
+module.exports = services;
